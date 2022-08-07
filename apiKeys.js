3243f59290c69c830202efcb408902e1d45e6e88
Wendell <wendellmorais3319@gmail.com>
2022-08-07 11:59:52 -0300

    build: variáveis de ambiente para deploy

---
+++ apiKeys.js
@@ -0,0 +1,10 @@
+export const apiKeys = {
+    apiKey: "apiKey",
+    authDomain: "domain",
+    projectId: "id",
+    storageBucket: "sotageBUcket",
+    messagingSenderId: "messagingSenderId",
+    appId: "appId"
+};
+
+export default apiKeys;
