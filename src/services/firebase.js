7012bac5ed38c7aef067a22c192feb21241a810c
Wendell <wendellmorais3319@gmail.com>
2022-08-07 13:51:45 -0300

    configurando variáveis de ambiente

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,17 +1,14 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
-import apiKeys from "../../apiKeys";
-
-const { apiKey, authDomain, projectId, storageBucket, messagingSenderId, appId } = apiKeys;
 
 const firebaseConfig = {
-    apiKey: apiKey,
-    authDomain: authDomain,
-    projectId: projectId,
-    storageBucket: storageBucket,
-    messagingSenderId: messagingSenderId,
-    appId: appId,
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
